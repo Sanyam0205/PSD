a3f2c8e7e7f5178e29f7137efb9568889436fc8b
Sanyam0205 <sanyamsingh0205@gmail.com>
2024-07-04 14:00:10 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userroutes');
+const userRoutes = require('./routes/userRoutes');
 
 
 const app = express();
