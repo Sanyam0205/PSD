39723b4f69b69f097cc78c343a3d48e7f53a86f1
Sanyam0205 <sanyamsingh0205@gmail.com>
2024-07-04 14:01:23 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,6 @@ mongoose.connect(process.env.MONGO_URI, {
 }).catch(err => console.error(err));
 
 app.use('/api/vendors', require('./routes/vendorRoutes'));
-app.use('/api/project-orders', require('./routes/ProjectOrderRoutes'));
+app.use('/api/project-orders', require('./routes/projectOrderRoutes'));
 app.use('/api/users', userRoutes);
 // app.use('/api/users', require('./routes/userRoutes'));
